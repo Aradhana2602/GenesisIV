8e4c3ba4f4a240d5c110abb287a011e6939a7647
Aradhana Kumari <aradhanakumari2602@gmail.com>
2025-02-07 19:58:15 +0530

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Home from './pages/home'
 import Sponsors from './pages/sponsors'
+import About from './pages/about'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,9 @@ function App() {
     <>
     
       <Home/>
+      <About />
       <Sponsors/>
+      
     </>
   )
 }
---
+++ src/pages/about.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './about.css'
+
+const About = () => {
+  return (
+    <div>
+      <div className="para">
+        <h1>About Genesis</h1>
+        <p>
+          Get ready to embark on the ultimate gaming adventure! Prepare to dive into a world where<br/>
+          every move counts, every decision shapes your destiny, and every challenge brings you<br/>
+          closer to victory! Welcome to GENESIS, where gamers unite, adrenaline runs high, and epic<br/>
+          moments are born. Are you ready to unleash your gaming prowess and make history?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;

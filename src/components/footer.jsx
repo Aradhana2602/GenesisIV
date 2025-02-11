756dff06f262d83a33c9b2b4df72ceef06e991a0
Aradhana Kumari <aradhanakumari2602@gmail.com>
2025-02-11 22:30:57 +0530

    resolving error

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { motion } from "framer-motion";
-import "./Footer.css";
+import "./footer.css";
 
 export default function Footer() {
   return (
